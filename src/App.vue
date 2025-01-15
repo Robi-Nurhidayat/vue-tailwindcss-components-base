9cfd12167f3701d9051ae188736f13cd507db81f
Robi Nurhidayat <robivanpersie290899@gmail.com>
2025-01-15 09:17:56 +0700

    finish login

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
-<script setup></script>
+<script setup>
+
+import Login from './components/Login.vue';
+
+
+</script>
 
 <template>
-  <h1 className="text-3xl font-bold underline">Hello world!</h1>
+
+<Login />
+
 </template>
---
+++ src/components/Login.vue
@@ -0,0 +1,27 @@
+<script setup>
+
+</script>
+
+<template>
+
+<section class="flex justify-center items-center h-screen bg-gray-100">
+    <div class=" w-[500px] border bg-white px-5 pt-5 pb-6 rounded">
+        <h1 class="mb-10 text-3xl font-semibold text-center text-gray-800">Login Dahulu</h1>
+        <form action="" class="flex flex-col gap-y-2">
+
+            <div class="flex flex-col gap-y-1">
+                <label for="email " class="text-2xl text-gray-800">Email</label>
+                <input type="text" class="outline-none border px-2 py-2 rounded text-lg text-gray-600">
+            </div>
+            <div class="flex flex-col gap-y-1">
+                <label for="password " class="text-2xl text-gray-800">Password</label>
+                <input type="text" class="outline-none border px-2 py-2 rounded text-lg text-gray-600">
+            </div>
+            <div class="pt-4">
+                <button class="bg-blue-500 hover:bg-blue-600 w-full py-2 text-white text-2xl font-semibold rounded">Login</button>
+            </div>
+        </form>
+    </div>
+</section>
+
+</template>
